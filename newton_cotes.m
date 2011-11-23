c77b1d88eef691a3ffe4d9a44e366fe702e4e418
Gabriel Ebner <gebner@2b7e.org>
2011-11-23 22:27:02 +0100

    newton_cotes: Add inline documentation.

--- newton_cotes.m
+++ newton_cotes.m
@@ -1,3 +1,9 @@
+% I = newton_cotes(f, a,b, degree, num_points)
+%
+% Integrates the function f on the interval [a,b] using the Newton-Cotes
+% formula of the specified degree and (approximately) num_points evaluation
+% points.
+%
 function I = newton_cotes(f, a,b, d, n),
 n = ceil((n-d-1)/d);
 w = d * (vander((0:d)/d)' \ (1./fliplr(1:d+1))')';
